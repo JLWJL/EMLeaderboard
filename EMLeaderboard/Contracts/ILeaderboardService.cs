e001a2b54e6296735c9e8f94afba3598659771e4
junlong <wjlinnz@gmail.com>
2025-02-20 23:18:52 +1300

    define ILeaderboardService interface

---
+++ EMLeaderboard/Contracts/ILeaderboardService.cs
@@ -0,0 +1,10 @@
+﻿using EMLeaderboard.Models;
+
+namespace EMLeaderboard.Contracts;
+
+public interface ILeaderboardService
+{
+     public Task<decimal> UpdateScoreAsync(long customerId, decimal scoreDelta);
+     public Task<List<CustomerScoreRank>> GetCustomersByRank(int? start, int? end);
+     public Task<List<CustomerScoreRank>> GetCustomersById(long customerId, decimal? high, decimal? low);
+}
---
+++ EMLeaderboard/Models/CustomerScoreRank.cs
@@ -0,0 +1,8 @@
+﻿namespace EMLeaderboard.Models;
+
+public class CustomerScoreRank
+{
+    public long CustomerId { get; set; }
+    public decimal Score { get; set; }
+    public int Rank { get; set; }
+}
