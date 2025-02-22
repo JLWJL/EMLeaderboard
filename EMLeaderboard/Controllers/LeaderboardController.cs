89dd719fa6a15d0daf55d7cdea468d68a9f15e1f
junlong <wjlinnz@gmail.com>
2025-02-23 10:26:49 +1300

    inject service to controller & update tests

--- EMLeaderboard/Controllers/LeaderboardController.cs
+++ EMLeaderboard/Controllers/LeaderboardController.cs
@@ -1,4 +1,5 @@
 using EMLeaderboard.Models;
+using EMLeaderboard.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EMLeaderboard.Controllers;
@@ -7,14 +8,16 @@ namespace EMLeaderboard.Controllers;
 [Route("[controller]")]
 public class LeaderboardController : ControllerBase
 {
-    public LeaderboardController()
+    private readonly ILeaderboardService _leaderboardService;
+
+    public LeaderboardController(ILeaderboardService leaderboardService)
     {
-        
+        _leaderboardService = leaderboardService;
     }
 
     [HttpPost]
     [Route("~/customer/{customerId}/score/{score}")]
-    public ActionResult<decimal> UpdateScore([FromRoute] long customerId, [FromRoute] decimal score)
+    public async Task<ActionResult<decimal>> UpdateScore([FromRoute] long customerId, [FromRoute] decimal score)
     {
         if(customerId < 1)
         {
@@ -25,31 +28,38 @@ public class LeaderboardController : ControllerBase
         {
             return BadRequest("Score must be between -1000 and 1000");
         }
+
+        var result = await _leaderboardService.UpdateScoreAsync(customerId, score);
         
-        return Ok(1000m);
+        return Ok(result);
     }
 
     [HttpGet]
     [Route("")]
-    public ActionResult<List<Customer>> GetCustomersByRank([FromQuery] int start, [FromQuery] int end)
+    //TODO: return type is {customerid, score, rank}
+    public async Task<ActionResult<List<CustomerScoreRank>>> GetCustomersByRank([FromQuery] int start, [FromQuery] int end)
     {
         if (start < 0 || end < 0 || start > end)
         {
             return BadRequest("Invalid start or end");
         }
 
-        return Ok(new List<Customer>());
+        var result = await _leaderboardService.GetCustomersByRankAsync(start, end);
+
+        return Ok(result);
     }
 
     [HttpGet]
     [Route("{customerId}")]
-    public ActionResult<List<Customer>> GetCustomersById([FromRoute] long customerId, [FromQuery] decimal? high = 0, [FromQuery] decimal? low = 0)
+    public async Task<ActionResult<List<Customer>>> GetCustomersById([FromRoute] long customerId, [FromQuery] int high = 0, [FromQuery] int low = 0)
     {
         if(high < 0 || low < 0 )
         {
             return BadRequest("{high} and {low} must be greater than 0");
         }
 
-        return Ok(new List<Customer>());
+        var result = await _leaderboardService.GetCustomersByIdAsync(customerId, high, low);
+
+        return Ok(result);
     }
 }
--- Tests/EMLeaderboard.UnitTests/Controllers/LeaderboardControllerTests.cs
+++ Tests/EMLeaderboard.UnitTests/Controllers/LeaderboardControllerTests.cs
@@ -1,137 +1,157 @@
-﻿using EMLeaderboard.Controllers;
+﻿using EMLeaderboard.Contracts;
+using EMLeaderboard.Controllers;
 using EMLeaderboard.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestPlatform.Common.Utilities;
+using Moq;
 
 namespace EMLeaderboard.UnitTests.Controllers;
 
 public class LeaderboardControllerTest
 {
     private readonly LeaderboardController _leaderboardController;
-    
+    private readonly Mock<ILeaderboardService> _leaderboardServiceMock;
     public LeaderboardControllerTest()
     {
-        _leaderboardController = new();
+        _leaderboardServiceMock = new Mock<ILeaderboardService>();
+        _leaderboardController = new LeaderboardController(_leaderboardServiceMock.Object);
     }
 
+    #region UpdateScore
     [Fact]
-    public void UpdateScore_ReturnsCurrentScoreOfDecimalType()
+    public async Task UpdateScore_WithValidParameters_CallsServiceUpdateScoreAsync()
     {
-        const long customerId = 111111111;
         //Arrange
+        const long customerId = 111111111;
         const decimal scoreChange = 100;
-        
+        _leaderboardServiceMock.Setup(x => x.UpdateScoreAsync(customerId, scoreChange)).ReturnsAsync(100m);
+
         //Act
-        var result = _leaderboardController.UpdateScore(customerId, scoreChange);
+        var result = await _leaderboardController.UpdateScore(customerId, scoreChange);
 
         //Assert
         var okResult = Assert.IsType<OkObjectResult>(result.Result);
-        Assert.IsType<decimal>(okResult.Value);
+        _leaderboardServiceMock.Verify(x => x.UpdateScoreAsync(customerId, scoreChange), Times.Once);
+    }
+
+     [Theory]
+    [InlineData(-1000)]
+    [InlineData(0)]
+    [InlineData(1000)]
+    public async Task UpdateScore_WithEdgeValidScore_CallsServiceUpdateScoreAsync(decimal score)
+    {
+        // Arrange
+        const long customerId = 111111111;
+        _leaderboardServiceMock.Setup(x => x.UpdateScoreAsync(customerId, score)).ReturnsAsync(score);
+
+        // Act
+        var result = await _leaderboardController.UpdateScore(customerId, score);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        _leaderboardServiceMock.Verify(x => x.UpdateScoreAsync(customerId, score), Times.Once);
     }
     
     [Theory]
     [InlineData(0)]
     [InlineData(-1)]
-    public void UpdateScore_WithInvalidCustomerId_ReturnsBadRequest(long customerId)
+    public async Task UpdateScore_WithInvalidCustomerId_ReturnsBadRequest(long customerId)
     {
         // Arrange
         const decimal scoreChange = 100;
 
         // Act
-        var result = _leaderboardController.UpdateScore(customerId, scoreChange);
+        var result = await _leaderboardController.UpdateScore(customerId, scoreChange);
 
         // Assert
         var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
         Assert.Equal("Customer ID must be greater than 0", badRequestResult.Value!.ToString());
+        _leaderboardServiceMock.Verify(x => x.UpdateScoreAsync(customerId, scoreChange), Times.Never);
     }
 
-    [Theory]
-    [InlineData(-1000)]
-    [InlineData(0)]
-    [InlineData(1000)]
-    public void UpdateScore_WithValidScore_ReturnsExpectedScore(decimal score)
-    {
-        // Arrange
-        const long customerId = 111111111;
-
-        // Act
-        var result = _leaderboardController.UpdateScore(customerId, score);
-
-        // Assert
-        var okResult = Assert.IsType<OkObjectResult>(result.Result);
-        Assert.Equal(1000m, okResult.Value);
-    }
+   
 
     [Theory]
     [InlineData(-1001)]
     [InlineData(1001)]
-    public void UpdateScore_WithInvalidScore_ReturnsBadRequest(decimal score)
+    public async Task UpdateScore_WithInvalidScore_ReturnsBadRequest(decimal score)
     {
         // Arrange
         const long customerId = 111111111;
 
         // Act
-        var result = _leaderboardController.UpdateScore(customerId, score);
+        var result = await _leaderboardController.UpdateScore(customerId, score);
 
         // Assert
         var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
         Assert.Contains("Score must be between -1000 and 1000", badRequestResult.Value!.ToString());
+        _leaderboardServiceMock.Verify(x => x.UpdateScoreAsync(customerId, score), Times.Never);
     }
+    #endregion
 
+    #region GetCustomersByRank
     [Theory]
     [InlineData(0, 10)]
     [InlineData(0, 0)]
     [InlineData(10, 20)]
-    public void GetCustomersByRank_WithValidRange_ReturnsOkResult(int start, int end)
+    public async Task GetCustomersByRank_WithValidRange_ReturnsOkResult(int start, int end)
     {
         // Act
-        var result = _leaderboardController.GetCustomersByRank(start, end);
+        var result = await _leaderboardController.GetCustomersByRank(start, end);
 
         // Assert
-        var okResult = Assert.IsType<OkObjectResult>(result.Result);
-        var customers = Assert.IsType<List<Customer>>(okResult.Value);
-        Assert.NotNull(customers);
+        Assert.IsType<OkObjectResult>(result.Result);
+        _leaderboardServiceMock.Verify(x => x.GetCustomersByRankAsync(start, end), Times.Once);
     }
 
     [Theory]
     [InlineData(-1, 10)]
     [InlineData(0, -1)]
     [InlineData(10, 5)]
-    public void GetCustomersByRank_WithInvalidRange_ReturnsBadRequest(int start, int end)
+    public async Task GetCustomersByRank_WithInvalidRange_ReturnsBadRequest(int start, int end)
     {
         // Act
-        var result = _leaderboardController.GetCustomersByRank(start, end);
+        var result = await _leaderboardController.GetCustomersByRank(start, end);
 
         // Assert
         var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
         Assert.Contains("Invalid start or end", badRequestResult.Value!.ToString());
+        _leaderboardServiceMock.Verify(x => x.GetCustomersByRankAsync(start, end), Times.Never);
     }
+    #endregion
 
+    #region GetCustomersById
     [Theory]
     [InlineData(111111111, 0, 0)]
     [InlineData(111111111, 100, 50)]
     [InlineData(111111111, 1000, 0)]
-    public void GetCustomersById_WithValidParameters_ReturnsOkResult(long customerId, decimal high, decimal low)
+    public async Task GetCustomersById_WithValidParameters_ReturnsOkResult(long customerId, int high, int low)
     {
+        //Arrange
+        _leaderboardServiceMock.Setup((x) => x.GetCustomersByIdAsync(It.IsAny<long>(), It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(new List<CustomerScoreRank>());
+        
         // Act
-        var result = _leaderboardController.GetCustomersById(customerId, high, low);
+        var result = await _leaderboardController.GetCustomersById(customerId, high, low);
 
         // Assert
         var okResult = Assert.IsType<OkObjectResult>(result.Result);
-        var customers = Assert.IsType<List<Customer>>(okResult.Value);
-        Assert.NotNull(customers);
+        Assert.IsType<List<CustomerScoreRank>>(okResult.Value);
+        _leaderboardServiceMock.Verify(x => x.GetCustomersByIdAsync(customerId, high, low), Times.Once);
     }
 
     [Theory]
     [InlineData(111111111, -1, 0)]
     [InlineData(111111111, 0, -1)]
     [InlineData(111111111, -10, -5)]
-    public void GetCustomersById_WithNegativeRanges_ReturnsBadRequest(long customerId, decimal high, decimal low)
+    public async Task GetCustomersById_WithNegativeRanges_ReturnsBadRequest(long customerId, int high, int low)
     {
         // Act
-        var result = _leaderboardController.GetCustomersById(customerId, high, low);
+        var result = await _leaderboardController.GetCustomersById(customerId, high, low);
 
         // Assert
         var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
         Assert.Contains("must be greater than 0", badRequestResult.Value!.ToString());
+        _leaderboardServiceMock.Verify(x => x.GetCustomersByIdAsync(customerId, high, low), Times.Never);
     }
+    #endregion
 }
